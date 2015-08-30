d41b868803608e6da8c4c46d5b95aad0c1a373f0
Fernando Freire <dogonthehorizon@gmail.com>
2015-08-30 08:35:45 -0700

    Add unit tests for mash-infusion, refactor initial-infusion tests

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/dogonthehorizon/maltose"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/math.combinatorics "0.1.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace  "0.2.10"]]
                    :source-paths ["dev/"]}})
--- src/maltose/mash.clj
+++ src/maltose/mash.clj
@@ -24,13 +24,3 @@
     (/  (* (- target-temperature current-temperature)
            (+  (* 0.20 grain-weight) current-volume))
        (- swp target-temperature))))
-
-;; TODO: figure out how to update the recipe model to include water/grain ratio
-;; TODO: rethink this implementation
-(defn mash-schedule
-  "Determine initial strike water temperature and subsequent boiling water
-  infusions to achieve the given mash rests."
-  [recipe grain-temperature first-rest & rests]
-  (let [grist-weight (reduce + (vals (:grist recipe)))]
-    {:strike-water-temperature (initial-infusion grain-temperature first-rest)
-     :rests rests}))
