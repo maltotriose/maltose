550401f45b0e883d41fb06149cdbc0cb0a0c72c2
Fernando Freire <dogonthehorizon@gmail.com>
2015-09-05 20:43:23 -0700

    Remove old grist calculation functions

--- src/maltose/core.clj
+++ src/maltose/core.clj
@@ -6,22 +6,6 @@
 (defrecord Recipe
   [batch-size og fg grist hop-schedule])
 
-(defn gravity-points
-  "Calculate the gravity points for a given recipe."
-  [recipe]
-  (* (* (- (:og recipe) 1) 1000) (:batch-size recipe)))
-
-(defn grist-weight
-  "Returns the total weight of a recipe's grist."
-  [brewhouse recipe]
-  (/ (gravity-points recipe) (* (:efficiency brewhouse) 36)))
-
-(defn grist-amounts
-  "Given a brewhouse and recipe, determine the weights of malts in the grist."
-  [brewhouse recipe]
-  (let [total-weight (grist-weight brewhouse recipe)]
-    (into {} (map (fn [[k v]] [k (* total-weight v)]) (:grist recipe)))))
-
 (defn grist-percentages
   "Given a map of grist weights, return a map of grist percentages."
   [grist]
